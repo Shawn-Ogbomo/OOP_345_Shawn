62954df6d542772a5028fd150bc9dcae03cc84a6
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2023-02-23 02:51:47 -0500

    working on Util module

---
+++ WS05/part 1/Util.cpp
@@ -0,0 +1,20 @@
+//Filename: Util.cpp
+//Name:	Shawn Ogbomo
+// Student #022609127
+// date: 02/19/2023
+//I have done all the coding by myself and only copied the code
+//that my professor provided to complete my workshops and assignments
+//-----------------------------------------------------------
+#include "Util.h"
+
+int Util::count(const std::vector<std::string>& words, const std::string& word) {
+	auto num_words = 0;
+	if (!words.empty()) {
+		for (const auto& internal_word : words) {
+			if (word == internal_word) {
+				++num_words;
+			}
+		}
+	}
+	return num_words;
+}
---
+++ WS05/part 1/Util.h
@@ -0,0 +1,15 @@
+//Filename: Util.h
+//Name:	Shawn Ogbomo
+// Student #022609127
+// date: 02/19/2023
+//I have done all the coding by myself and only copied the code
+//that my professor provided to complete my workshops and assignments
+//-----------------------------------------------------------
+#ifndef SDDS_Util_H_
+#define SDDS_Util_H_
+#include <string>
+#include <vector>
+namespace Util {
+	int count(const std::vector<std::string>& words, const std::string& word);
+}
+#endif//!SDDS_Util_H
